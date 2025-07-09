e24e26534047f2baabf9f626112eaa43146697dc
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-07-09 14:24:14 -0700

    Create a custom hook that fetches data from the MLB Stats API

---
+++ src/hooks/useGameData.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const useGameData = (url: string) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+          setError(error.message);
+          console.log('API Error:', {
+            status: error.response?.status,
+            url: error.config?.url,
+            method: error.config?.method,
+            message: error.message,
+          });
+        } else if (error instanceof Error) {
+          setError(error.message);
+          console.log('Unexpected error:', error.message);
+        } else {
+          setError('An unknown error occurred');
+          console.log('Unknown error:', error);
+        }
+      } finally {
+        setLoading(false);
+        setError(null);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return {
+    data,
+    loading,
+    error,
+  };
+};
+
+export default useGameData;
